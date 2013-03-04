db353d3b491cb851129ee57a2a8beb1243687a11
carolinag <carolina.acgg@gmail.com>
2013-03-04 11:13:13 -0300

    New project created

---
+++ Camaleao/Camaleao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace Camaleao
+{
+    class Program
+    {
+        public static string AbsolutePath =  Path.GetDirectoryName (Assembly.GetExecutingAssembly().Location);
+        static void Main(string[] args)
+        {
+            Image camaleao = Image.FromFile(Path.Combine (AbsolutePath, "../../Images/camaleao.bmp"));
+            Image camaleao2 = (Image)camaleao.Clone();
+
+            camaleao2.Save(Path.Combine (AbsolutePath,"../../Images/camaleao2.bmp"));
+
+        }
+    }
+}
